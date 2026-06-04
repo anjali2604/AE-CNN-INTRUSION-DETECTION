7b25db29be830c62ffb35972beb1958b9ff00c28
ANJALI <anjalisonaboina@gmail.com>
2026-06-04 22:29:59 +0530

    Implement training function for AutoEncoder
    Added training function for AutoEncoder model with basic setup.

--- models/train_autoencoder.py
+++ models/train_autoencoder.py
@@ -1,1 +1,19 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from models.autoencoder import AutoEncoder
 
+def train():
+    model = AutoEncoder()
+
+    criterion = nn.MSELoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+    print("Training Autoencoder Model...")
+
+    # Training logic will be added here
+
+    print("Training completed.")
+
+if __name__ == "__main__":
+    train()
